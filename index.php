370068d2a926113322e72004c8dca48c629a20d3
AshiraLOL <ashiraEX_online@hotmail.com>
2023-10-16 13:38:35 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Minecraft CPM catalog models</title>
+    <meta name="viewport" content="width=device-width">
+    <meta charset="UTF-8">
+    <meta name="description" content="Website catalog 3D model for mod minecraft CPM , create by AshiraEX">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+</head>
+
+<body>
+
+    <div id="main_body">
+
+        <div id="top_header">
+        A catalog for Minecraft Mod called <a href="https://github.com/tom5454/CustomPlayerModels">Customizable Player Models</a></div>
+        <div id="findtag">
+            <label for="input_box_tag">input_box_tag</label>
+            <input id="input_box_tag" type="text" placeholder="Search item" onchange="do_filter()">
+        </div>
+
+
+        <!-- <div onclick="prev_page()">BACK</div>
+<div onclick="next_page()">NEXT</div> -->
+
+       <!--  <div id="blank_space"></div> -->
+
+        <div id="info_1">       
+        This website is not owned by the mod owner (tom5454) and was created for search purposes only. If you encounter any problems on the website please contact me instead.
+        </div>
+
+
+        <div id="fev_toggle" onclick="only_myfev_toggle()">show favorites only</div>
+        <div id="fev_toggle_empty">No favorites found. (Click the heart icon to favorite)</div>
+
+
+        <div id="box_content">
+
+        </div>
+
+    </div>
+
+
+    <div id="blank_space"></div>
+
+    <div id="blank_bottom_space"></div>
+
+
+    <div class="content content--canvas">
+				<h2 class="content__title"></h2>
+	</div>
+
+
+</body>
+
+
+
+
+</html>
+
+
+<link rel="preload" href="css/FiraCode-VariableFont_wght.ttf" as="font" type="font/ttf" crossorigin />
+<link rel="stylesheet" href="css/v1.css" />
+
+<script src="js/jquery-3.7.1.min.js"></script>
+
+
+<script src="js/main.js"></script>
+
+<!-- <script src="js/Ambient Canvas Backgrounds by crnacura/js/noise.min.js"></script>
+<script src="js/Ambient Canvas Backgrounds by crnacura/js/util.js"></script>
+<script src="js/Ambient Canvas Backgrounds by crnacura/js/coalesce.js"></script> -->
+
---
+++ js/main.js
@@ -0,0 +1,213 @@
+$(document).ready(function () {
+	check_mobile();
+});
+
+function check_mobile() {
+	const detectDeviceType = () =>
+		/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+			navigator.userAgent
+		)
+			? "Mobile"
+			: "Desktop";
+	if (detectDeviceType() == "Mobile") {
+		are_u_sure_gonna_open_this_web();
+	} else {
+		call_data();
+	}
+}
+
+function are_u_sure_gonna_open_this_web() {
+	if (localStorage.getItem("not_first_time_huh") == null) {
+		alert(
+			"It looks like you're accessing the website via a mobile device. This website has pictures of many models. Recommend using unlimited mobile data."
+		);
+		localStorage.setItem("not_first_time_huh", "yeah..");
+	}
+	call_data();
+}
+
+page = 0;
+function call_data(v) {
+	if (!v) {
+		v = "";
+	}
+
+/* 	$.post(
+		"datagen.php",	
+		{
+			filter: v,
+			offset: page,
+		},
+
+		function (data, status) {
+			$("#box_content").html(data);
+			load_list();				
+		},
+		"html"
+	); */
+
+	postData("datalist.php", {  }).then((data) => {
+		$("#box_content").html(data);
+		load_list();		
+});
+
+}
+
+
+async function postData(url = "", data = {}) {
+	// Default options are marked with *
+	const response = await fetch(url, {
+	  method: "POST", // *GET, POST, PUT, DELETE, etc.
+	  mode: "cors", // no-cors, *cors, same-origin
+	  cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+	  credentials: "same-origin", // include, *same-origin, omit
+	  headers: {
+		"Content-Type": "application/json",
+		// 'Content-Type': 'application/x-www-form-urlencoded',
+	  },
+	  redirect: "follow", // manual, *follow, error
+	  referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+	  body: JSON.stringify(data), // body data type must match "Content-Type" header
+	});
+	return response.text(); // parses JSON response into native JavaScript objects
+  }
+
+
+
+function clear_fev_list() {
+	localStorage.removeItem("list");
+	$(".model_botton").removeClass("fev");
+}
+
+
+function do_filter() {
+
+	set_filter = $("#input_box_tag").val();
+	$(".model_box").addClass("do_filter_hide");
+
+	
+	owner_list = document.querySelectorAll(".owner_name");
+
+	owner_list.forEach(function (index) {
+		var get_owner_name = $(index).html();
+
+		if (get_owner_name.toLowerCase().search(set_filter.toLowerCase()) > 0) {
+			$(index).closest(".model_box").removeClass("do_filter_hide");
+		}
+
+		if (set_filter == "") {
+			$(index).closest(".model_box").removeClass("do_filter_hide");
+		}
+	});
+
+
+	name_item = document.querySelectorAll(".name_item");	
+
+	name_item.forEach(function (index) {
+		var get_name_item = $(index).html();
+
+		
+
+		if (get_name_item.toLowerCase().search(set_filter.toLowerCase()) > 0) {
+			$(index).closest(".model_box").removeClass("do_filter_hide");
+			/* console.log(get_name_item+" "+set_filter); */
+		}
+
+		if (set_filter == "") {
+			$(index).closest(".model_box").removeClass("do_filter_hide");
+		}
+	});	
+
+
+
+}
+
+function load_list() {
+	let locallist_ary = [];
+
+	if (localStorage.getItem("list") !== null) {
+		locallist_ary = JSON.parse(localStorage.getItem("list"));
+	}
+
+	for (let i = locallist_ary.length - 1; i >= 0; i--) {
+		$("#fav_id_" + locallist_ary[i]).addClass("fev");
+	}
+}
+
+function save_list(v) {
+	let locallist_ary = [];
+
+	var val_save = v.dataset.id;
+	var find_dup = 0;
+
+	if (localStorage.getItem("list") !== null) {
+		locallist_ary = JSON.parse(localStorage.getItem("list"));
+	}
+
+	for (let i = locallist_ary.length - 1; i >= 0; i--) {
+		if (locallist_ary[i] === val_save) {
+			locallist_ary.splice(i, 1);
+			find_dup = 1;
+		}
+	}
+
+	if (find_dup == 0) {
+		locallist_ary.push(val_save);
+		$("#" + v.id).addClass("fev");
+	} else {
+		$("#" + v.id).removeClass("fev");
+	}
+
+	/* console.log(v.id); */
+
+	locallist_ary = JSON.stringify(locallist_ary);
+	localStorage.setItem("list", locallist_ary);
+
+	/* console.log(val_save);
+	console.log(locallist_ary); */
+
+	show_list = document.querySelectorAll(".fev");
+}
+
+/* function next_page() {
+	page++;
+	call_data();
+}
+
+function prev_page() {
+	if (page != 0) {
+		page--;
+		call_data();
+	}
+} */
+
+var only_myfev_toggle_s = 0;
+
+var show_list;
+
+function only_myfev_toggle() {
+	if (only_myfev_toggle_s == 0) {
+		show_list = document.querySelectorAll(".fev");
+
+		/* $(".model_box").css("display", "none"); */
+		$(".model_box").addClass("do_fev_hide");		
+
+		show_list.forEach(function (index) {
+			/* console.log($(index).closest(".model_box")); */
+			$(index).closest(".model_box").removeClass("do_fev_hide");
+		});
+		only_myfev_toggle_s = 1;
+	} else {
+		$(".model_box").removeClass("do_fev_hide");
+		only_myfev_toggle_s = 0;		
+	}
+
+
+	if(show_list.length==0&& only_myfev_toggle_s==1){
+		$("#fev_toggle_empty").addClass("show_el");
+	}else{
+		$("#fev_toggle_empty").removeClass("show_el");
+	}
+
+
+}
