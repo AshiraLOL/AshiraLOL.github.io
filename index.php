9ae207ffa2082ca866b6ecd5fef5fd50160a73c9
AshiraLOL <ashiraEX_online@Hotmail.com>
2024-04-04 14:02:15 +0700

    update

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>RTAF : ระบบรายงานยอดประจำวัน ทอ.</title>
+    <meta name="description" content="เว็บกรอกข้อมูลใช้สำหรับองค์กรณ์ภายในเท่านั้น">
+	<meta name="author" content="Ashira Junlouloung">
+    <meta name="viewport" content="width=device-width">
+    <meta charset="UTF-8">
+    <meta name="description" content="Website catalog 3D model for mod minecraft CPM , create by AshiraEX">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    
+    <!-- <link rel="preload" href="css/FiraCode-VariableFont_wght.ttf" as="font"/> -->
+    <link rel="stylesheet" href="css/normalize.css?v=1">
+    <link rel="stylesheet" href="css/style.css" />  
+    <script src="js/jquery-3.7.1.min.js"></script>
+    <script src="js/main.js?v=123"></script>  
+
+
+</head>
+
+<body>
+
+
+</body>
+
+
+
+</html>
